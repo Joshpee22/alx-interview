b76d38c67270141b4e0fb8554a6ca970caeda39b
joshpee22 <owobujoshua2017@gmail.com>
2024-02-15 10:26:06 +0100

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,39 @@
 #!/usr/bin/node
+
 /**
-  prints all characters of a Star Wars movie
-  */
+ * Prints all characters of a Star Wars movie
+ */
 
 const myArgs = process.argv.slice(2);
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
 
 request(url, async function (error, response, body) {
-  if (!error) {
-    const json = JSON.parse(body);
-    const endpoints = json.characters;
-    for (const endpoint of endpoints) {
-      await new Promise(function (resolve, reject) {
-        request(endpoint, function (error, response, body) {
-          if (!error) {
-            console.log(JSON.parse(body).name);
-            resolve();
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  const json = JSON.parse(body);
+  const endpoints = json.characters;
+
+  // Use Promise.all for concurrent requests
+  await Promise.all(endpoints.map(async (endpoint) => {
+    try {
+      const characterBody = await new Promise((resolve, reject) => {
+        request(endpoint, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(body);
           }
         });
       });
+
+      console.log(JSON.parse(characterBody).name);
+    } catch (error) {
+      console.error('Error fetching character:', error);
     }
-  }
+  }));
 });
+
